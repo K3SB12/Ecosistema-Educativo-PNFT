773fff777df733662346ec187e9c9dde896b97ee
K3SB12 <143125631+K3SB12@users.noreply.github.com>
2026-03-17 11:27:52 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Ecosistema-Educativo-PNFT/', // Cambiado
+  base: '/Ecosistema-Educativo-PNFT/',
   server: {
     port: 3000,
     open: true
