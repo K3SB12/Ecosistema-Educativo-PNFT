715012fe50e85c9f35577f2c15d5255de01800a1
K3SB12 <143125631+K3SB12@users.noreply.github.com>
2026-03-17 10:26:18 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pnft-articulador/',
+  base: '/pnft-articulador/', // Ajusta según el nombre de tu repo
+  server: {
+    port: 3000,
+    open: true
+  },
   build: {
     outDir: 'dist',
     sourcemap: true
