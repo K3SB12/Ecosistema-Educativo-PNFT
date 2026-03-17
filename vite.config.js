a00b1b37cb6b8b6f9f99b3dd5b42567b8b93d486
K3SB12 <143125631+K3SB12@users.noreply.github.com>
2026-03-17 11:12:15 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pnft-articulador/', // Ajusta según el nombre de tu repo
+  base: '/Ecosistema-Educativo-PNFT/', // Cambiado
   server: {
     port: 3000,
     open: true
