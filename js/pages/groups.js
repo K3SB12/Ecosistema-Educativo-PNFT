69a2d6e919021ab7688a48b4733e7931db1ee7df
K3SB12 <143125631+K3SB12@users.noreply.github.com>
2026-03-18 08:02:19 -0600

    Create groups.js

---
+++ js/pages/groups.js
@@ -0,0 +1,7 @@
+// js/pages/groups.js
+(function() {
+    function render() {
+        return `<div class="recent-section"><h2>Grupos (en construcción)</h2></div>`;
+    }
+    window.Groups = { render };
+})();
